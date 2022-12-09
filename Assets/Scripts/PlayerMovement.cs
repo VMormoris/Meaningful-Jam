07db2f95b8d5e75b09babdcf4ff9bad7f8351ded
VMormoris <vmormoris2014@gmail.com>
2022-12-09 22:37:52 +0100

    Finalizing movement :crossed_fingers:

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,8 +13,9 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb;
     public Animator animator;
 
-    private Vector2 mDir;
-    private bool mSliding;
+    public Vector2 mDir;
+    public bool mSliding;
+    public bool mHasCollided = false;
 
     // Update is called once per frame
     void Update()
@@ -27,13 +28,25 @@ public class PlayerMovement : MonoBehaviour
                 mDir.y = Input.GetAxisRaw("Vertical");
             mSliding = false;
         }
-        else if (mDir.x != 0.0f && mDir.y != 0.0f && Colliding())
+        else if(mDir.x != 0.0f || mDir.y != 0)
+            mSliding = true;
+
+        if (!mHasCollided && Colliding())
         {
             mSliding = false;
             mDir = new Vector2(0.0f, 0.0f);
+            mHasCollided = true;
         }
-        else
-            mSliding = true;
+        else if(mHasCollided)
+        {
+            mDir.y = 0.0f;
+            mDir.x = Input.GetAxisRaw("Horizontal");
+            if (mDir.x == 0.0f)
+                mDir.y = Input.GetAxisRaw("Vertical");
+            mSliding = !CanWalk();
+            if(mDir.x != 0.0f || mDir.y != 0.0f)
+                mHasCollided = false;
+        }
 
         animator.SetFloat("horizontal", mDir.x);
         animator.SetFloat("vertical", mDir.y);
