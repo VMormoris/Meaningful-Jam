ae323931d8b868e4329fd4f821b3243c936845bd
ManourasM <1manman1@windowslive.com>
2022-12-10 20:21:11 +0200

    MainMenu and Settings 0.1

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //Load next scene from Scenes in build
+    }
+    public void GoToSettingsMenu()
+    {
+        SceneManager.LoadScene("SettingsScene");
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene(0); //loads first scene(MenuScene)
+    }
+    public void Quitgame()
+    {
+        Application.Quit();
+    }
+    
+}
